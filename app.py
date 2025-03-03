7a269c8da07145dd20f7df5ad8a181154e19b8d9
Navara Sai Divakar <119742639+SaiDivakar-Navara@users.noreply.github.com>
2025-03-03 15:53:51 +0530

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-import os
 
 app = FastAPI()
 
 # Enable CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change to specific domain in production
+    allow_origins=["*"],  # Change to a specific domain in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 # Replace with your Gemini API key
-API_KEY = "Paste your API Key Here"
+API_KEY = "Paste your api-key Here"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
 @app.post("/generate")
@@ -26,5 +25,4 @@ async def generate(prompt: dict):
     }
     response = requests.post(API_URL, json=data, headers=headers)
     return response.json()
-
 
